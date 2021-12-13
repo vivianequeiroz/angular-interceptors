5adae67d1803c97a775acca999fa091c1e59455c
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-12-13 17:59:59 -0300

    feat: add tailwind css to the project

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  mode: "jit",
+  purge: [
+    {
+      enabled: true,
+      content: ["./src/**/*.{html,ts}"],
+    },
+  ],
+  darkMode: "media", // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
