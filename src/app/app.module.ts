e9e424dd6847b0cde835e88ef077ac588a0a9edc
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-12-14 18:48:44 -0300

    fix: add tailwind.css in styles of angualr.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HeaderComponent } from './components/header/header.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -8,7 +9,13 @@ import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
-  declarations: [AppComponent, BooksListComponent, LoginComponent, RegisterComponent],
+  declarations: [
+    AppComponent,
+    BooksListComponent,
+    LoginComponent,
+    RegisterComponent,
+    HeaderComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
